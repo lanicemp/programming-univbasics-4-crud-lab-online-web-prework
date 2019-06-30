4a0ce898f1dd0e68f405ba26b72167dfdacc2772
LaNice Powell <lanicemp@aol.com>
2019-06-30 19:37:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -33,7 +33,7 @@ end
 
 def retrieve_first_element_from_array(array)
   number_one = ["wow", "one","two", "three"]
-  number_one [-1]
+  number_one [0]
 end
 
 def retrieve_last_element_from_array(array)
